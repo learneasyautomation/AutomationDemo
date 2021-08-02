4e7d94db98596915a06d39f3ce551536080283dc
learneasyautomation <learneasyautomation@gmail.com>
2021-08-02 21:55:13 +0530

    PT-1234 - First commit

---
+++ AutomationDemo1/src/test/java/flipkart/TestLogin.java
@@ -0,0 +1,12 @@
+package flipkart;
+
+public class TestLogin {
+
+	
+	public static void main(String[] args){
+		System.out.println("Welcome Pruthvi to automation training");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/win10/IdeaProjects/com.automation/src/driver/chromedriver.exe");
+		//WebDriver driver=new ChromeDriver();
+	
+	}
+}
