fe5312fb964b5d077c5f2fd9b559bd7415ed61b2
learneasyautoamtion <learneasyautoamtion@gmail.com>
2021-08-02 21:57:57 +0530

    added few lines in login page

--- AutomationDemo1/src/test/java/flipkart/TestLogin.java
+++ AutomationDemo1/src/test/java/flipkart/TestLogin.java
@@ -2,9 +2,10 @@ package flipkart;
 
 public class TestLogin {
 
-	
+	//test the login functionality
 	public static void main(String[] args){
 		System.out.println("Welcome Pruthvi to automation training");
+		
 		System.setProperty("webdriver.chrome.driver", "C:/Users/win10/IdeaProjects/com.automation/src/driver/chromedriver.exe");
 		//WebDriver driver=new ChromeDriver();
 	
